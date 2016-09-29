ab27c54b30dd5515241d102c1ec535829601fb7c
aimsam <1thblood@gmail.com>
2016-09-29 18:50:21 +0800

    update php7

--- images/boot2docker/nfs-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# osx nfs settings
-sudo -s
-echo "nfs.server.mount.require_resv_port = 0" >> /etc/nfs.conf
-touch /etc/exports
-echo "/Users -mapall=501:20" >> /etc/exports
-nfsd enable
-nfsd restart
-exit
